86e6ada6d152f897c2cce3de733b5486ebd6343d
Eric Lowry <ericlowry14@gmail.com>
2026-07-21 15:02:57 +0200

    Small formatting fix

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -5,6 +5,8 @@ export default {
 	...enData,
 	// Engine Notifications
 	NOTICE__APPLIED_CHANGES: (actionName: string, count: number) =>
-		enData.NOTICE__APPLIED_CHANGES.replace('{0}', actionName).replace('{1}', count.toString()).replace('{2}', count === 1 ? '' : 's'),
+		enData.NOTICE__APPLIED_CHANGES.replace('{0}', actionName)
+			.replace('{1}', count.toString())
+			.replace('{2}', count === 1 ? '' : 's'),
 	NOTICE__ERROR: (message: string) => enData.NOTICE__ERROR.replace('{0}', message),
 };
--- src/locales/fr.ts
+++ src/locales/fr.ts
@@ -5,6 +5,8 @@ export default {
 	...frData,
 	// Engine Notifications
 	NOTICE__APPLIED_CHANGES: (actionName: string, count: number) =>
-		frData.NOTICE__APPLIED_CHANGES.replace('{0}', actionName).replace('{1}', count.toString()).replace('{2}', count === 1 ? '' : 's'),
+		frData.NOTICE__APPLIED_CHANGES.replace('{0}', actionName)
+			.replace('{1}', count.toString())
+			.replace('{2}', count === 1 ? '' : 's'),
 	NOTICE__ERROR: (message: string) => frData.NOTICE__ERROR.replace('{0}', message),
 };
